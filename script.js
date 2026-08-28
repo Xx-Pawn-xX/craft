3e5eb28987db36d0a3d94783031a0a50f7791d99
Xx-Pawn-xX <chesspawnchillin@outlook.com>
2026-08-28 12:50:42 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById('ping');
-const out = document.getElementById('out');
-let n = 0;
-btn.addEventListener('click', () => {
-  n++;
-  out.textContent = `Clicked ${n} time${n === 1 ? '' : 's'} ✨`;
-  console.log('click', n);
-});
-console.log('Welcome to XCODX! Edit code on the left and watch it run instantly.');
